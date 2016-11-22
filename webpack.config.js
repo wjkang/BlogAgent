7a2b58548985a93e1a4dab0b24ff67394854c084
username <977865769@qq.com>
2016-11-22 22:37:19 +0800

    update

--- webpack.config.js
+++ webpack.config.js
@@ -1,1 +1,21 @@
-1
+module.exports = {
+    entry: {
+        content:'./content.js',
+    },
+    output: {
+        path: __dirname + '/dist',
+        filename: 'js/[name].js'
+    },
+    module: {
+        loaders: [
+            {
+                test: /\.js$/,
+                exclude: /(node_modules)/,
+                loader: 'babel',
+                query: {
+                    presets: ['es2015'],
+                }
+            }
+        ]
+    }
+};
