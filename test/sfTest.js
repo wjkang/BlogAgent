2e8f48aee27f091fe34863692f68a310f5372fc3
username <977865769@qq.com>
2016-12-18 15:57:25 +0800

    update

--- test/sfTest.js
+++ test/sfTest.js
@@ -2,22 +2,6 @@
 var sf = require("../src/sf/doPost");
 var http=require("http");
 var query = require("querystring");
-var data = {
-    cookie:"Hm_lvt_e23800c454aa573c0ccb16b52665ac26=1480042218,1480049341,1480050350,1480056908; sf_remember=585ebc30c1ae3b05a4633c777219f2f4; __jsluid=ee6b8a36e03924b1a60b54e683709183; Hm_lpvt_e23800c454aa573c0ccb16b52665ac26=1480057545; PHPSESSID =web6~ro0cd50592ceiqp65isfimid26",
-    data:{
-        title:"后端自动添加21212",
-        text:"本文章由工具自动发布",
-        blogId:1200000006808837,
-        tags:[1040000000089436,1040000000089434],
-
-    }
-
-}
-/*sf.doPost(config).then(data=> {
-    //console.log(data);
-}).catch(ex=> {
-    //console.log(ex);
-});*/
 var server=new http.Server();
 server.on('request', function(req, res){
     if(req.method=="POST"){
